bcedb7267ab9c338ac89e38ea1cb466766135f3f
rashmiojha <rashmi.ojha@tamu.edu>
2018-11-28 16:45:22 -0600

    Minor changes in data reader

--- src/data_reader.py
+++ src/data_reader.py
@@ -5,7 +5,7 @@ import pickle
 from glob import glob
 from sklearn.model_selection import train_test_split
 
-path = '../sample/hmdb'
+path = '../sample/ucf101'
 data = []
 label = []
 cnt=0
@@ -16,7 +16,6 @@ for entry in os.scandir(path):
         for input in os.scandir(path + "/" + entry.name):
             impath = path + "/" + entry.name + "/" + input.name + "/" + 'n_frames'
             val = int(open(impath, 'r').read())
-            print(val)
             min_frame = min(min_frame, val)
 
 for entry in os.scandir(path):
@@ -24,10 +23,9 @@ for entry in os.scandir(path):
         for input in os.scandir(path + "/" + entry.name):
             data_inner = []
             total_current_frame = int(open(path + "/" + entry.name + "/" + input.name + '/n_frames','r').read())
-            l = (total_current_frame - min_frame)/2
+            l = (total_current_frame - min_frame)//2
             for i in range(l,l+min_frame):
                 impath = glob(path + "/" + entry.name + "/" + input.name + "/" + 'image_*'+str(i)+'.jpg')[0]
-                print (impath)
                 img = Image.open(impath)
                 img.load()
                 img_np = np.asarray(img, dtype="int32")
@@ -41,13 +39,11 @@ label = np.asarray(label)
 print("Data: ",data.shape)
 print("Label: ",label.shape)
 
-x_train,y_train,x_test,y_test = train_test_split(data,label,shuffle=True)
-# x_train= data[:200]
-# y_train = data[:200]
-# x_test = data[200:]
-# y_test = label[200:]
+x_train, x_test, y_train, y_test = train_test_split(data,label,shuffle=True)
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
 
 save = (x_train, y_train), (x_test, y_test)
 
-with open('../sample/hmdb/data.pkl','wb') as f:
+with open('../sample/data_new.pkl','wb') as f:
     pickle.dump(save, f)
