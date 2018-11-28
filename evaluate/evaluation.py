b47d08fb4b68f8cbe468d193ca636a00ad6c8334
Mayank Sharma <mayank.sharma@tamu.edu>
2018-11-28 16:36:39 -0600

    Implemented basic Hill Climbing (Greedy) Search

--- evaluate/evaluation.py
+++ evaluate/evaluation.py
@@ -5,10 +5,10 @@ from src.video_classifier import VideoClassifier
 
 def evaluate(path, supervised):
     (x_train, y_train), (x_test, y_test) = pickle.load(open(path, 'rb'))
-    supervised.fit(x_train, y_train, time_limit = 15 * 60)
+    supervised.fit(x_train, y_train, time_limit=5*60)
     supervised.final_fit(x_train, y_train, x_test, y_test, retrain=True)
     return supervised.evaluate(x_test, y_test)
 
 
 if __name__ == '__main__':
-    print("Accuracy: ",evaluate('../sample/sample/video_classification_data', VideoClassifier()))
+    print("Accuracy: ", evaluate('../sample/data.pkl', VideoClassifier()))
--- src/video_classifier.py
+++ src/video_classifier.py
@@ -2,9 +2,12 @@ import os
 import torch
 import numpy as np
 from skimage.transform import resize
+from sklearn.model_selection import train_test_split
 from functools import reduce
+from queue import Queue
+import time
 
-from simple_nn import Net
+from .simple_nn import Net
 
 from autokeras.supervised import Supervised
 from autokeras.preprocessor import OneHotEncoder, ImageDataTransformer
@@ -12,15 +15,18 @@ from autokeras.nn.model_trainer import ModelTrainer
 from autokeras.nn.loss_function import classification_loss
 from autokeras.nn.metric import Accuracy
 from autokeras.utils import pickle_to_file, pickle_from_file
+from autokeras.nn.generator import CnnGenerator
 
 
 class VideoClassifier(Supervised):
     def __init__(self):
         super().__init__(verbose=False)
         self.labels = None
-        self.net = Net()
+        self.net = None
         self.augment = None
-        self.Epochs = 2
+        self.Length = 3
+        self.Width = 4
+        self.Epochs = 10
         self.encoder = OneHotEncoder()
         self.path = '../temp'
 
@@ -42,16 +48,12 @@ class VideoClassifier(Supervised):
         predicted = self.encoder.inverse_transform(output)
         return predicted
 
-    def fit(self, x_train=None, y_train=None, time_limit=None):
+    def fit(self, x_train=None, y_train=None, time_limit=60*60*6):
+        end_time = time.time()+time_limit
+
         x_train = self.preprocess(x_train)
-
-        x_valid, y_valid = [], []
-        x_valid.append(x_train[0])
-        x_valid.append(x_train[15])
-        y_valid.append(y_train[0])
-        y_valid.append(y_train[15])
-
-        x_train, y_train = np.array(x_train[1:15]), np.array(y_train[1:15])
+        x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.25, shuffle=True)
+        x_train, y_train = np.array(x_train), np.array(y_train)
         x_valid, y_valid = np.array(x_valid), np.array(y_valid)
 
         self.encoder.fit(y_train)
@@ -62,31 +64,58 @@ class VideoClassifier(Supervised):
         train_data = self.data_transformer.transform_train(x_train, y_train, batch_size=1)
         test_data = self.data_transformer.transform_test(x_valid, y_valid, batch_size=1)
 
-        self.model_trainer = ModelTrainer(self.net,
-                                     path=self.path,
-                                     loss_function=classification_loss,
-                                     metric=Accuracy,
-                                     train_data=train_data,
-                                     test_data=test_data,
-                                     verbose=True)
-
-        # Searching for best epoch as a hyperparameter
-        accuracy = 0
         y_valid = self.encoder.inverse_transform(y_valid)
-        for epoch in [2, 4, 6]:
-            self.model_trainer.train_model(epoch, 2)
-            outputs = []
-            with torch.no_grad():
-                for index, (inputs, _) in enumerate(test_data):
-                    outputs.append(self.net(inputs).numpy())
-            output = reduce(lambda x, y: np.concatenate((x, y)), outputs)
-            pred_valid = self.encoder.inverse_transform(output)
-            accu = self.metric().evaluate(y_valid, pred_valid)
 
-            if accu > accuracy:
-                self.Epochs = epoch
+        trainingQ = []
+        trainingQ.append((self.Length, self.Width, self.Epochs))
+        accuracy = 0.0
+
+        while trainingQ:
+            inc = False
+            for len, width, epoch in trainingQ:
+                if time.time() < end_time:
+                    net = CnnGenerator(self.encoder.n_classes, x_train.shape[1:])\
+                        .generate(model_len=len, model_width=width).produce_model()
+
+                    model_trainer = ModelTrainer(net,
+                                             path=self.path,
+                                             loss_function=classification_loss,
+                                             metric=Accuracy,
+                                             train_data=train_data,
+                                             test_data=test_data,
+                                             verbose=True)
+                    model_trainer.train_model(epoch, 3)
+
+                    outputs = []
+                    with torch.no_grad():
+                        for index, (inputs, _) in enumerate(test_data):
+                            outputs.append(net(inputs).numpy())
+                    output = reduce(lambda x, y: np.concatenate((x, y)), outputs)
+                    pred_valid = self.encoder.inverse_transform(output)
+                    accu = self.metric().evaluate(y_valid, pred_valid)
+
+                    if accu > accuracy:
+                        self.Epochs = epoch
+                        self.Length = len
+                        self.Width = width
+                        accuracy = accu
+                        inc = True
+
+            if not inc:
+                break
+
+            # Create children
+            trainingQ = []
+            trainingQ.append((self.Length+1, self.Width, self.Epochs))
+            trainingQ.append((self.Length, self.Width*2, self.Epochs))
+            trainingQ.append((self.Length, self.Width, self.Epochs+5))
 
         print('Finished Fit')
+        print("Optimal Conv3D Network Parameters:")
+        print("Number of Layers (Length):", self.Length)
+        print("Number of Filters (Width):", self.Width)
+        print("Numbre of Epochs", self.Epochs)
+
 
     def final_fit(self, x_train, y_train, x_test, y_test, trainer_args=None, retrain=True):
         x_train = self.preprocess(x_train)
@@ -100,6 +129,9 @@ class VideoClassifier(Supervised):
         train_data = self.data_transformer.transform_train(x_train, y_train, batch_size=1)
         test_data = self.data_transformer.transform_test(x_test, y_test, batch_size=1)
 
+        self.net = CnnGenerator(self.encoder.n_classes, x_train.shape[1:]) \
+            .generate(model_len=self.Length, model_width=self.Width).produce_model()
+
         pickle_to_file(self, os.path.join(self.path, 'classifier'))
 
         self.model_trainer = ModelTrainer(self.net,
@@ -110,7 +142,7 @@ class VideoClassifier(Supervised):
                                           test_data=test_data,
                                           verbose=True)
 
-        self.model_trainer.train_model(self.Epochs, 2)
+        self.model_trainer.train_model(self.Epochs, 3)
         print('Finished Final Fit')
 
     def preprocess(self, data):
